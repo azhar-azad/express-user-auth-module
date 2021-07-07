408a1c43ba9be642acd8f32a6af2e0129f81224b
azhar-azad <azhar.u.azad@gmail.com>
2021-07-07 22:08:32 +0600

    initial server setup with db conn

---
+++ middlewares/error.mw.js
@@ -0,0 +1,39 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+  // Copying all properties of 'err' to 'error' using spread operator.
+  let error = { ...err };
+
+  // error.message = err.message;
+
+  // Log to console for dev
+  console.log(err);
+  console.log(`err.name: ${err.name}`);
+
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered ${JSON.stringify(err.keyValue)}`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(value => value.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res
+    .status(error.statusCode || 500)
+    .json({
+      success: false,
+      error: error.message || 'Server Error'
+    });
+};
+
+module.exports = errorHandler;
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/error.mw');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+
+
+const app = express();
+
+/** Middlewares Start */
+
+// Body parser
+app.use(express.json());
+
+// DEV logging middlewares
+if (process.env.APP_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+
+
+// Handle errors
+app.use(errorHandler);
+
+/** Middlewares End */
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.APP_ENV} mode on port ${PORT}`.yellow.bold);
+});
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
